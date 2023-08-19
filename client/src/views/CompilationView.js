4704e51a8c702100f70f8b488d95916adcbf76d2
Peter Martin <pg.martin@rcn.com>
2023-08-19 18:09:56 -0400

    logic for displaying reviews, added bootstrap

--- client/src/views/CompilationView.js
+++ client/src/views/CompilationView.js
@@ -1,21 +1,25 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { ReviewService } from "../providers/ReviewService";
 import { useNavigate, Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 const CompilationView = () => {
-  let reviews;
+  let [reviews, setReviews] = useState([]);
 
   useEffect(() => {
     ReviewService.listReviews()
     .then(response => response.json())
     .then(data => {
-      reviews = data;
-
-      //debug
-      console.log('reviews', reviews);
+      setReviews(data);
     })
     .catch(err => console.error(err));
   }, []);
+
+      //debug
+      console.log('reviews', reviews);
 
   return (
     <>
@@ -29,6 +33,26 @@ const CompilationView = () => {
         Below are informal reviews from visitors to this site containing their overall
         observations and impressions after sampling a vintage.
       </p>
+
+      <div className="reviews_wrapper">
+          <Container>
+            <Row>
+              { (reviews.length == 0) && <h5>No reviews yet</h5> }
+              { reviews.map(review => (
+                    <Col md={6}>
+                      <div className="review">
+                        <p className="region">{review.region}</p>
+                        <p>{review.producer} {review.year}</p>
+                        <p className="notes">{review.notes}</p>
+                        <a href="#">Update</a>
+                        <a href="#">Delete</a>
+                      </div>
+                    </Col>
+                ))
+              }
+            </Row>
+          </Container>
+      </div>      
     </>
   );
 };
